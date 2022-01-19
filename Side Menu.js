62353619f8dc4b217f170e3c30933e01218f55cd
KanishaL <73332154+KanishaLiyanage@users.noreply.github.com>
2022-01-19 15:08:17 +0530

    Add side menu files

---
+++ Side Menu.js
@@ -0,0 +1,28 @@
+let menuToggle = document.querySelector('.toggle');
+let SideMenu = document.querySelector('.SideMenu');
+
+menuToggle.onclick = function(){
+
+    menuToggle.classList.toggle('active')
+    SideMenu.classList.toggle('active')
+    
+}
+
+let list = document.querySelectorAll('.list');
+
+for (let i=0; i,list.length; i++) {
+
+    list[i].onclick = function() {
+
+        let j = 0;
+
+        while(j < list.length) {
+
+            list[j++].className = 'list';
+
+        }
+
+        list[i].className = 'list active';
+
+    }
+}
